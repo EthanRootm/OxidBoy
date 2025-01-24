2ee3f574d43188e7c7e8ea7b3439aa3a0b741657
EthanRootm <143615568+EthanRootm@users.noreply.github.com>
2025-01-24 10:21:04 -0500

    adding pause functionallity

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,9 @@ use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use sdl2::pixels::PixelFormatEnum;
 use OxidBoy::sdl2::{load_font, update_with_buffer};
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
@@ -129,6 +132,7 @@ fn main() {
 
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     loop {
         // Stop the program, if the GUI is closed by the user
         if !window.is_open() {
@@ -138,6 +142,8 @@ fn main() {
         // Execute an instruction
         mbrd.next();
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
     //Change these Controls to what you want
@@ -177,7 +183,11 @@ fn main() {
             }
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             window.update_with_buffer(window_buffer.as_slice(), SCREEN_W, SCREEN_H).unwrap();
+=======
+            let _ = update_with_buffer(&mut canvas, &mut texture, &window_buffer, SCREEN_W, pause, &texture_creator, _scale, &ttf_context, font_path);
+>>>>>>> Stashed changes
 =======
             let _ = update_with_buffer(&mut canvas, &mut texture, &window_buffer, SCREEN_W, pause, &texture_creator, _scale, &ttf_context, font_path);
 >>>>>>> Stashed changes
